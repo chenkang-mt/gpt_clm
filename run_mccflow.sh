2de5bd478b0a75604f24e779b6684470443eb425
zedong-mt <gaozedong@gaozedongdeMacBook-Pro.local>
2023-05-30 15:50:38 +0800

    feat:add automatic restart

--- run_mccflow.sh
+++ run_mccflow.sh
@@ -8,10 +8,12 @@ python3 train_hvd_clm.py \
     --model_name_or_path gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-103-raw-v1 \
-    --per_device_train_batch_size 3 \
-    --per_device_eval_batch_size 3 \
-    --checkpointing_steps 1000 \
-    --output_dir $DATA/clm-output \
+    --block_size 512 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --checkpointing_steps 5000 \
+    --output_dir $DATA/output \
     --cache_dir $DATA/cache \ 
     --from_checkpoint_meta $DATA/checkpoints \
-    --log_dir $DATA/logs
+    --log_dir $DATA/logs \
+    --resume_from_checkpoint 1
